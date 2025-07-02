576778c4d87c75cffc5e11e9d0f83994a885047c
Sabhay Thakkar <sabhaythakkar02@gmail.com>
2025-07-02 20:55:00 +0530

    fixed issue

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './footer.css';
+import './Footer.css';
 import youtube_icon from '../../assets/youtube_icon.png';
 import twitter_icon from '../../assets/twitter_icon.png';
 import instagram_icon from '../../assets/instagram_icon.png';
